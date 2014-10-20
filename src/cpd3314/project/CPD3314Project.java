3f706080567b2f1f96ed3e1a239fd712cf7826cc
Len Payne <len.payne@lambtoncollege.ca>
2014-10-20 12:33:24 -0400

    Initial Commit

---
+++ src/cpd3314/project/CPD3314Project.java
@@ -0,0 +1,17 @@
+package cpd3314.project;
+
+/**
+ * Final Term Project for CPD-3314 Java Development I
+ *
+ * @author <ENTER YOUR NAME HERE>
+ */
+public class CPD3314Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/cpd3314/project/Product.java
@@ -0,0 +1,9 @@
+package cpd3314.project;
+
+/**
+ * 
+ * @author <ENTER YOUR NAME HERE>
+ */
+public class Product {
+    
+}
