6aea428d42b19a5980f6c8ed2ae46d64a1ae8a79
Len Payne <len.payne@lambtoncollege.ca>
2014-10-21 12:25:43 -0400

    Finished Tests and Expected Results

---
+++ test/cpd3314/project/CPD3314ProjectTest.java
@@ -0,0 +1,289 @@
+/*
+ * Copyright 2014 Len Payne <len.payne@lambtoncollege.ca>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cpd3314.project;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.FileSystems;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+import static org.custommonkey.xmlunit.XMLAssert.assertXMLEqual;
+import static org.custommonkey.xmlunit.XMLAssert.assertXMLEqual;
+import static org.custommonkey.xmlunit.XMLAssert.assertXMLEqual;
+import static org.custommonkey.xmlunit.XMLAssert.assertXMLEqual;
+import static org.custommonkey.xmlunit.XMLAssert.assertXMLEqual;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.junit.After;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+import org.yaml.snakeyaml.Yaml;
+
+/**
+ * @author Len Payne <len.payne@lambtoncollege.ca>
+ */
+public class CPD3314ProjectTest {
+
+    private ByteArrayOutputStream outContent;
+    private ByteArrayOutputStream errContent;
+
+    public CPD3314ProjectTest() {
+    }
+
+    @Before
+    public void setUp() {
+        outContent = new ByteArrayOutputStream();
+        errContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(null);
+        System.setErr(null);
+    }
+
+    private void assertFilesEqual(File a, File b) throws IOException {
+        Scanner aIn = new Scanner(a);
+        Scanner bIn = new Scanner(b);
+        while (aIn.hasNext() && bIn.hasNext()) {
+            assertEquals(aIn.nextLine().trim(), bIn.nextLine().trim());
+        }
+        assertTrue("Files Not Equal Length", !aIn.hasNext() && !bIn.hasNext());
+    }
+
+    private void assertXMLFilesEqual(File a, File b) throws IOException, SAXException {
+        Scanner aIn = new Scanner(a);
+        Scanner bIn = new Scanner(b);
+        StringBuilder aXML = new StringBuilder();
+        StringBuilder bXML = new StringBuilder();
+        while (aIn.hasNext() && bIn.hasNext()) {
+            aXML.append(aIn.nextLine().trim());
+            bXML.append(bIn.nextLine().trim());
+        }
+        assertTrue("Files Not Equal Length", !aIn.hasNext() && !bIn.hasNext());
+        assertXMLEqual(aXML.toString(), bXML.toString());
+    }
+
+    private void assertJSONFilesEqual(File a, File b) throws IOException {
+        Scanner aIn = new Scanner(a);
+        Scanner bIn = new Scanner(b);
+        StringBuilder aJSON = new StringBuilder();
+        StringBuilder bJSON = new StringBuilder();
+        while (aIn.hasNext() && bIn.hasNext()) {
+            aJSON.append(aIn.nextLine().trim());
+            bJSON.append(bIn.nextLine().trim());
+        }
+        assertTrue("Files Not Equal Length", !aIn.hasNext() && !bIn.hasNext());
+        JSONObject aJ = (JSONObject) JSONValue.parse(aJSON.toString());
+        JSONObject bJ = (JSONObject) JSONValue.parse(bJSON.toString());
+        assertEquals(aJ.toJSONString(), bJ.toJSONString());
+    }
+
+    private void assertYAMLFilesEqual(File a, File b) throws IOException {
+        Yaml yaml1 = new Yaml();
+        Yaml yaml2 = new Yaml();
+        FileInputStream aIS = new FileInputStream(a);
+        FileInputStream bIS = new FileInputStream(b);
+        Iterable<Object> aYAML = yaml1.loadAll(aIS);
+        Iterable<Object> bYAML = yaml2.loadAll(bIS);
+        Set<Object> aSet = new HashSet<>();
+        Set<Object> bSet = new HashSet<>();
+        for (Object x : aYAML) {
+            aSet.add(x);
+        }
+        for (Object y : bYAML) {
+            bSet.add(y);
+        }
+        assertEquals("Files Not Equal Length", aSet.size(), bSet.size());
+        assertTrue("A does not contain all B", aSet.containsAll(bSet));
+        assertTrue("B does not contain all A", bSet.containsAll(aSet));
+    }
+
+    @Test
+    public void testNoArg() throws Exception {
+        System.out.println("main");
+        String[] args = {};
+        CPD3314Project.main(args);
+        File expected = FileSystems.getDefault().getPath("test", "noArg.xml").toFile();
+        File result = new File("CPD3314.xml");
+        assertTrue("Output File Doesn't Exist", result.exists());
+        assertXMLFilesEqual(expected, result);
+    }
+
+    @Test
+    public void testFormatXML() throws Exception {
+        System.out.println("main");
+        String[] args = {"-format=XML"};
+        CPD3314Project.main(args);
+        File expected = FileSystems.getDefault().getPath("test", "formatXML.xml").toFile();
+        File result = new File("CPD3314.xml");
+        assertTrue("Output File Doesn't Exist", result.exists());
+        assertXMLFilesEqual(expected, result);
+    }
+
+    @Test
+    public void testFormatJSON() throws Exception {
+        System.out.println("main");
+        String[] args = {"-format=JSON"};
+        CPD3314Project.main(args);
+        File expected = FileSystems.getDefault().getPath("test", "formatJSON.json").toFile();
+        File result = new File("CPD3314.json");
+        assertTrue("Output File Doesn't Exist", result.exists());
+        assertJSONFilesEqual(expected, result);
+    }
+
+    @Test
+    public void testFormatYAML() throws Exception {
+        System.out.println("main");
+        String[] args = {"-format=YAML"};
+        CPD3314Project.main(args);
+        File expected = FileSystems.getDefault().getPath("test", "formatYAML.yaml").toFile();
+        File result = new File("CPD3314.yaml");
+        assertTrue("Output File Doesn't Exist", result.exists());
+        assertYAMLFilesEqual(expected, result);
+    }
+    
+    @Test
+    public void testFormatSQL() throws Exception {
+        System.out.println("main");
+        String[] args = {"-format=SQL"};
+        CPD3314Project.main(args);
+        File expected = FileSystems.getDefault().getPath("test", "formatSQL.sql").toFile();
+        File result = new File("CPD3314.sql");
+        assertTrue("Output File Doesn't Exist", result.exists());
+        assertFilesEqual(expected, result);
+    }
+    
+    @Test
+    public void testFormatHTML() throws Exception {
+        System.out.println("main");
+        String[] args = {"-format=HTML"};
+        CPD3314Project.main(args);
+        File expected = FileSystems.getDefault().getPath("test", "formatHTML.html").toFile();
+        File result = new File("CPD3314.html");
+        assertTrue("Output File Doesn't Exist", result.exists());
+        assertXMLFilesEqual(expected, result);
+    }
+    
+    @Test
+    public void testOutput() throws Exception {
+        System.out.println("main");
+        String[] args = {"-o=test"};
+        CPD3314Project.main(args);
+        File expected = FileSystems.getDefault().getPath("test", "noArg.xml").toFile();
+        File result = new File("test.xml");
+        assertTrue("Output File Doesn't Exist", result.exists());
+        assertXMLFilesEqual(expected, result);
+    }
+
+    @Test
+    public void testLimitAndOutput() throws Exception {
+        System.out.println("main");
+        String[] args = {"-o=ten", "-limit=10"};
+        CPD3314Project.main(args);
+        File expected = FileSystems.getDefault().getPath("test", "limit10.xml").toFile();
+        File result = new File("ten.xml");
+        assertTrue("Output File Doesn't Exist", result.exists());
+        assertXMLFilesEqual(expected, result);
+    }
+    
+    @Test
+    public void testSortAAndLimitAndOutput() throws Exception {
+        System.out.println("main");
+        String[] args = {"-sort=A", "-o=a", "-limit=10"};
+        CPD3314Project.main(args);
+        File expected = FileSystems.getDefault().getPath("test", "sortA.xml").toFile();
+        File result = new File("a.xml");
+        assertTrue("Output File Doesn't Exist", result.exists());
+        assertXMLFilesEqual(expected, result);
+    }
+    
+    @Test
+    public void testSortIAndLimitAndOutput() throws Exception {
+        System.out.println("main");
+        String[] args = {"-sort=I", "-o=i", "-limit=10"};
+        CPD3314Project.main(args);
+        File expected = FileSystems.getDefault().getPath("test", "sortI.xml").toFile();
+        File result = new File("i.xml");
+        assertTrue("Output File Doesn't Exist", result.exists());
+        assertXMLFilesEqual(expected, result);
+    }
+    
+    @Test
+    public void testSortDAndLimitAndOutput() throws Exception {
+        System.out.println("main");
+        String[] args = {"-sort=D", "-o=d", "-limit=10"};
+        CPD3314Project.main(args);
+        File expected = FileSystems.getDefault().getPath("test", "sortD.xml").toFile();
+        File result = new File("d.xml");
+        assertTrue("Output File Doesn't Exist", result.exists());
+        assertXMLFilesEqual(expected, result);
+    }
+    
+    @Test
+    public void testGetIDAndOutput() throws Exception {
+        System.out.println("main");
+        String[] args = {"-getID=400", "-o=fourHundred"};
+        CPD3314Project.main(args);
+        File expected = FileSystems.getDefault().getPath("test", "get400.xml").toFile();
+        File result = new File("fourHundred.xml");
+        assertTrue("Output File Doesn't Exist", result.exists());
+        assertXMLFilesEqual(expected, result);
+    }
+    
+    @Test
+    public void testGetDateAndOutput() throws Exception {
+        System.out.println("main");
+        String[] args = {"-getDate=2013-02-09", "-o=byDate"};
+        CPD3314Project.main(args);
+        File expected = FileSystems.getDefault().getPath("test", "getDate.xml").toFile();
+        File result = new File("byDate.xml");
+        assertTrue("Output File Doesn't Exist", result.exists());
+        assertXMLFilesEqual(expected, result);
+    }
+    
+    @Test
+    public void testFindOneWordAndOutput() throws Exception {
+        System.out.println("main");
+        String[] args = {"-find=lorem", "-o=lorem"};
+        CPD3314Project.main(args);
+        File expected = FileSystems.getDefault().getPath("test", "findOne.xml").toFile();
+        File result = new File("lorem.xml");
+        assertTrue("Output File Doesn't Exist", result.exists());
+        assertXMLFilesEqual(expected, result);
+    }
+    
+    @Test
+    public void testFindMultiWordsAndOutput() throws Exception {
+        System.out.println("main");
+        String[] args = {"-find=dolor sit amet", "-o=dolor"};
+        CPD3314Project.main(args);
+        File expected = FileSystems.getDefault().getPath("test", "findMulti.xml").toFile();
+        File result = new File("dolor.xml");
+        assertTrue("Output File Doesn't Exist", result.exists());
+        assertXMLFilesEqual(expected, result);
+    }
+}
---
+++ test/cpd3314/project/ProductTest.java
@@ -0,0 +1,262 @@
+/*
+ * Copyright 2014 Len Payne <len.payne@lambtoncollege.ca>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cpd3314.project;
+
+import java.util.HashMap;
+import java.util.Map;
+import static org.custommonkey.xmlunit.XMLAssert.assertXMLEqual;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.yaml.snakeyaml.Yaml;
+
+/**
+ * @author Len Payne <len.payne@lambtoncollege.ca>
+ */
+public class ProductTest {
+
+    public ProductTest() {
+    }
+
+    private Product sample;
+    private final int sampleId = 1;
+    private final String sampleName = "Wood Screw";
+    private final String sampleDesc = "To screw wood.";
+    private final int sampleQuantity = 10;
+    private final String sampleDate = "2014-10-14";
+
+    @Before
+    public void setUp() {
+        sample = new Product(sampleId, sampleName, sampleDesc, sampleQuantity, sampleDate);
+    }
+
+    /**
+     * Test of getId method, of class Product.
+     */
+    @Test
+    public void testGetId() throws Exception {
+        System.out.println("getId");
+        Product instance = new Product();
+        int expResult = 0;
+        int result = instance.getId();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setId method, of class Product.
+     */
+    @Test
+    public void testSetId() throws Exception {
+        System.out.println("setId");
+        int id = 4;
+        Product instance = new Product();
+        instance.setId(id);
+        int result = instance.getId();
+        assertEquals(id, result);
+    }
+
+    /**
+     * Test of getName method, of class Product.
+     */
+    @Test
+    public void testGetName() throws Exception {
+        System.out.println("getName");
+        Product instance = new Product();
+        String expResult = "";
+        String result = instance.getName();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setName method, of class Product.
+     */
+    @Test
+    public void testSetName() throws Exception {
+        System.out.println("setName");
+        String name = "bob";
+        Product instance = new Product();
+        instance.setName(name);
+        String result = instance.getName();
+        assertEquals(name, result);
+    }
+
+    /**
+     * Test of getDescription method, of class Product.
+     */
+    @Test
+    public void testGetDescription() throws Exception {
+        System.out.println("getDescription");
+        Product instance = new Product();
+        String expResult = "";
+        String result = instance.getDescription();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setDescription method, of class Product.
+     */
+    @Test
+    public void testSetDescription() throws Exception {
+        System.out.println("setDescription");
+        String description = "bob";
+        Product instance = new Product();
+        instance.setDescription(description);
+        String result = instance.getDescription();
+        assertEquals(description, result);
+    }
+
+    /**
+     * Test of getQuantity method, of class Product.
+     */
+    @Test
+    public void testGetQuantity() throws Exception {
+        System.out.println("getQuantity");
+        Product instance = new Product();
+        int expResult = 0;
+        int result = instance.getQuantity();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setQuantity method, of class Product.
+     */
+    @Test
+    public void testSetQuantity() throws Exception {
+        System.out.println("setQuantity");
+        int quantity = 10;
+        Product instance = new Product();
+        instance.setQuantity(quantity);
+        int result = instance.getQuantity();
+        assertEquals(quantity, result);
+    }
+
+    /**
+     * Test of getDateAdded method, of class Product.
+     */
+    @Test
+    public void testGetDateAdded() throws Exception {
+        System.out.println("getDateAdded");
+        Product instance = new Product();
+        String expResult = "";
+        String result = instance.getDateAdded();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setDateAdded method, of class Product.
+     */
+    @Test
+    public void testSetDateAdded() throws Exception {
+        System.out.println("setDateAdded");
+        String dateAdded = "2014-10-14";
+        Product instance = new Product();
+        instance.setDateAdded(dateAdded);
+        String result = instance.getDateAdded();
+        assertEquals(dateAdded, result);
+    }
+
+    /**
+     * Test of toXML method, of class Product.
+     */
+    @Test
+    public void testToXML() throws Exception {
+        System.out.println("toXML");
+        Product instance = sample;
+        String result = instance.toXML().replaceAll(">[ \t\n]+<", "><");
+        String expected = String.format("<product><id>%d</id><name>%s</name>"
+                + "<description>%s</description><quantity>%d</quantity>"
+                + "<dateAdded>%s</dateAdded></product>",
+                sampleId, sampleName, sampleDesc, sampleQuantity, sampleDate);
+        assertXMLEqual(result, expected);
+    }
+
+    /**
+     * Test of toYAML method, of class Product.
+     */
+    @Test
+    public void testToYAML() throws Exception {
+        System.out.println("toYAML");
+        Product instance = sample;
+        String result = instance.toYAML();
+        Yaml yaml = new Yaml();
+        Map<String, Object> resultSet = (Map<String, Object>) yaml.load(result);
+        Map<String, Object> expectedSet = new HashMap<>();
+        expectedSet.put("id", sampleId);
+        expectedSet.put("name", sampleName);
+        expectedSet.put("description", sampleDesc);
+        expectedSet.put("quantity", sampleQuantity);
+        expectedSet.put("dateAdded", sampleDate);
+        for (String k : expectedSet.keySet()) {
+            assertTrue(resultSet.containsKey(k));
+            assertTrue(resultSet.get(k).equals(expectedSet.get(k)));
+        }
+    }
+
+    /**
+     * Test of toJSON method, of class Product.
+     */
+    @Test
+    public void testToJSON() throws Exception {
+        System.out.println("toJSON");
+        Product instance = sample;
+        JSONObject expected = new JSONObject();
+        expected.put("name", sampleName);
+        expected.put("id", sampleId);
+        expected.put("description", sampleDesc);
+        expected.put("quantity", sampleQuantity);
+        expected.put("dateAdded", sampleDate);
+        String result = instance.toJSON();
+        JSONObject resultJSON = (JSONObject) JSONValue.parse(result);
+        assertEquals(expected.toJSONString(), resultJSON.toJSONString());
+    }
+
+    /**
+     * Test of toSQL method, of class Product.
+     */
+    @Test
+    public void testToSQL() throws Exception {
+        System.out.println("toSQL");
+        Product instance = sample;
+        String expResult = String.format("INSERT INTO Products VALUES (%d, \"%s\", \"%s\", %d, \"%s\");",
+                sampleId, sampleName, sampleDesc, sampleQuantity, sampleDate);
+        String result = instance.toSQL();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of toHTML method, of class Product.
+     */
+    @Test
+    public void testToHTML() throws Exception {
+        System.out.println("toHTML");
+        Product instance = sample;
+        String[] expResult = {"<div class=\"product\">",
+            "<h1>Wood Screw</h1>",
+            "<p>ID: 1</p>",
+            "<p>To screw wood.</p>",
+            "<p>Quantity: 10</p>",
+            "<p>Added: 2014-10-14</p>",
+            "</div>"};
+        String[] result = instance.toHTML().replaceAll(">[ \t\n]+", ">!!").split("!!");
+        for (int i = 0; i < result.length; i++) {
+            assertEquals(result[i], expResult[i]);
+        }
+    }
+
+}
